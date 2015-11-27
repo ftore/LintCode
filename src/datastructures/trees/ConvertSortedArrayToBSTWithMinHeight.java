74c430b9c290a0a6f72f191bac55f33d9d1aeeb3
Rakhimov Akmal <ra07@kinx.net>
2015-11-27 10:01:18 +0900

    Coded "Convert Sorted Array to Binary Search Tree With Minimal Height problem"

---
+++ src/datastructures/trees/ConvertSortedArrayToBSTWithMinHeight.java
@@ -0,0 +1,26 @@
+package datastructures.trees;
+
+public class ConvertSortedArrayToBSTWithMinHeight {
+	/**
+     * @param A: an integer array
+     * @return: a tree node
+     */
+	public TreeNode sortedArrayToBST(int[] A) {
+		return sortedArrayToBST(A, 0, A.length - 1);
+	}
+	
+	public TreeNode sortedArrayToBST(int[] A, int start, int end) {
+		if(start > end) return null;
+		
+		// same as (start + end) / 2, avoids overflow
+		int mid = start + (end - start) / 2;
+		TreeNode node = new TreeNode(A[mid]);
+		node.left = sortedArrayToBST(A, start, mid - 1);
+		node.right = sortedArrayToBST(A, mid + 1, end);
+		return node;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
